021cb5e19b1aa67582cc87c680b505e8f411b70d
Gaurav Borhade <borhadegaurav0@gmail.com>
2025-03-15 18:33:21 +0530

    Trending movies

--- src/TrendingMovies.jsx
+++ src/TrendingMovies.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./App.css";
+import "./TrendingMovies.css";
 
 const TrendingMovies = () => {
   const [movies, setMovies] = useState([]);
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import App from './TrendingMovies.jsx'
 import MovieDetails from './MoviesDetail.jsx';
 
 createRoot(document.getElementById('root')).render(
@@ -14,6 +14,5 @@ createRoot(document.getElementById('root')).render(
         <Route path='/movie/:id' element={<MovieDetails/>}/>
       </Routes>
     </Router>
-    
   </StrictMode>
 )
